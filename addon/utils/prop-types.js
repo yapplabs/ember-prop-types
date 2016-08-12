b5c5a4e532df58184e8101f1ae91ff91d9aed79a
Matthew Dahl <dahl.matthew@gmail.com>
2016-08-12 06:17:33 -0700

    Fix tests

--- addon/utils/prop-types.js
+++ addon/utils/prop-types.js
@@ -154,8 +154,8 @@ validators.instanceOf = function (ctx, name, value, def, logErrors) {
   const valid = value instanceof type
 
   if (!valid && logErrors) {
-    const name = type.toString().match(/function (\w*)/)[1]
-    Ember.Logger.warn(`Expected property ${name} to be an instance of ${name}`)
+    const nameOfType = type.toString().match(/function (\w*)/)[1]
+    Ember.Logger.warn(`Expected property ${name} to be an instance of ${nameOfType}`)
   }
 
   return valid
