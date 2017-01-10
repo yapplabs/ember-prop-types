5f46d834c0121341977f21827c8b31c6c0de9c42
Matthew Dahl <dahl.matthew@gmail.com>
2017-01-10 10:25:37 -0800

    Add note

--- addon/utils/validators/ember-component.js
+++ addon/utils/validators/ember-component.js
@@ -11,6 +11,7 @@ export default function (ctx, name, value, def, logErrors, throwErrors) {
   const isObject = typeOf(value) === 'object'
 
   const valid = isObject && Object.keys(value).some((key) => {
+    // NOTE: this is based on internal API and thus could break without warning.
     return key.indexOf('COMPONENT_CELL') === 0
   })
 
