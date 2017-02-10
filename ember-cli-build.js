76b9b7c2b48f1b1bf6aea20fc9eb9f39813b36e5
Matthew Dahl <mdahl@ciena.com>
2017-02-09 16:54:54 -0800

    Work with latest pr-bumper

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -4,7 +4,9 @@ var EmberAddon = require('ember-cli/lib/broccoli/ember-addon')
 module.exports = function (defaults) {
   var app = new EmberAddon(defaults, {
     'babel': {
-      optional: ['es6.spec.symbols'],
+      optional: ['es6.spec.symbols']
+    },
+    'ember-cli-babel': {
       includePolyfill: true
     },
     'ember-cli-mocha': {
