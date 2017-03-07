996cb7485a7c82239ccc34b2bb4a66e031da9403
Seena <seenarowhani95@gmail.com>
2017-03-07 14:05:42 -0500

    Update typeOf to typeof

--- addon/utils/validators/array-of.js
+++ addon/utils/validators/array-of.js
@@ -2,13 +2,9 @@
  * The PropTypes.arrayOf validator
  */
 import logger from '../logger'
-import Ember from 'ember'
-
-const {typeOf} = Ember
 const {isArray} = Array
 
 export default function (validators, ctx, name, value, def, logErrors, throwErrors) {
-  const type = typeOf(value)
   const typeDef = def.typeDef
 
   const valid = isArray(value) && value.every((item, index) => {
@@ -16,7 +12,7 @@ export default function (validators, ctx, name, value, def, logErrors, throwErro
   })
 
   if (!valid && logErrors) {
-    logger.warn(ctx, `Expected property ${name} to be an array of type ${typeDef.type} but instead got: ${type}`, throwErrors)
+    logger.warn(ctx, `Expected property ${name} to be an array of type ${typeDef.type} but instead got: ${typeof value}`, throwErrors)
   }
 
   return valid
