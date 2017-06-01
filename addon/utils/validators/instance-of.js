7dc425b7215e1952af0392339629413c1da4bed0
Erik Restificar <erik.restificar@gmail.com>
2017-06-01 13:41:36 -0500

    fixed error with nameOfType.  typedef may not return function string.

--- addon/utils/validators/instance-of.js
+++ addon/utils/validators/instance-of.js
@@ -10,7 +10,9 @@ export default function (ctx, name, value, def, logErrors, throwErrors) {
   const valid = value instanceof type
 
   if (!valid && logErrors) {
-    const nameOfType = type.toString().match(/function (\w*)/)[1]
+    const typeString = type.toString()
+    const nameOfTypeMatch = typeString.match(/function (\w*)/)
+    const nameOfType = nameOfTypeMatch ? nameOfTypeMatch[1] : typeString
     logger.warn(
       ctx,
       `Expected property ${name} to be an instance of ${nameOfType} but instead got: ${typeOf(value)}`,
