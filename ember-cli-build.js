ab784292cfcb4166698f365a9de966a20c3f057a
Michael Carroll <juwara0@users.noreply.github.com>
2017-09-18 10:53:45 -0500

    upgrade to Ember CLI 2.15.1

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,8 +1,9 @@
 /* eslint-env node */
+'use strict'
 const EmberAddon = require('ember-cli/lib/broccoli/ember-addon')
 
 module.exports = function (defaults) {
-  var app = new EmberAddon(defaults, {
+  let app = new EmberAddon(defaults, {
     'babel': {
       optional: ['es6.spec.symbols']
     },
@@ -19,7 +20,15 @@ module.exports = function (defaults) {
       ],
       theme: 'okaidia'
     }
+    // Add options here
   })
+
+  /*
+    This build file specifies the options for the dummy test app of this
+    addon, located in `/tests/dummy`
+    This build file does *not* influence how the addon or the app using it
+    behave. You most likely want to be modifying `./index.js` or app's build file
+  */
 
   return app.toTree()
 }
--- testem.js
+++ testem.js
@@ -1,15 +1,24 @@
+/* eslint-env node */
 var Reporter = require('ember-test-utils/reporter')
 
 module.exports = {
   disable_watching: true,
   framework: 'mocha',
   launch_in_ci: [
+    'Chrome',
     'Firefox'
   ],
   launch_in_dev: [
-    'Firefox',
     'Chrome'
   ],
   reporter: new Reporter(),
-  test_page: 'tests/index.html?hidepassed'
+  test_page: 'tests/index.html?hidepassed',
+  browser_args: {
+    Chrome: [
+      '--disable-gpu',
+      '--headless',
+      '--remote-debugging-port=9222',
+      '--window-size=1440,900'
+    ]
+  }
 }
