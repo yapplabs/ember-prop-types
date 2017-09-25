21a78bf8f81739616f8f5bd6783a5d2fa6e79ed3
Michael Carroll <juwara0@users.noreply.github.com>
2017-09-25 11:58:09 -0500

    revert changes to testem.js

--- testem.js
+++ testem.js
@@ -1,16 +1,16 @@
 /* eslint-env node */
+var Reporter = require('ember-test-utils/reporter')
 
 module.exports = {
   disable_watching: true,
   framework: 'mocha',
   launch_in_ci: [
-    'Chrome',
     'Firefox'
   ],
   launch_in_dev: [
+    'Firefox',
     'Chrome'
   ],
-  reporter: 'tap',
-  tap_quiet_logs: true,
+  reporter: new Reporter(),
   test_page: 'tests/index.html?hidepassed'
 }
