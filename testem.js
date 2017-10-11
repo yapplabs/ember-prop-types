885ce0d7a1a0b2a5ab5c85fabea01b3e29531e3d
Michael Carroll <juwara0@users.noreply.github.com>
2017-10-11 09:21:14 -0500

    revert google chrome headless changes

--- testem.js
+++ testem.js
@@ -5,23 +5,12 @@ module.exports = {
   disable_watching: true,
   framework: 'mocha',
   launch_in_ci: [
-    'Chrome',
     'Firefox'
   ],
   launch_in_dev: [
+    'Firefox',
     'Chrome'
   ],
   reporter: new Reporter(),
-  test_page: 'tests/index.html?hidepassed',
-  browser_args: {
-    Chrome: {
-      mode: 'ci',
-      args: [
-        '--disable-gpu',
-        '--headless',
-        '--remote-debugging-port=9222',
-        '--window-size=1440,900'
-      ]
-    }
-  }
+  test_page: 'tests/index.html?hidepassed'
 }
