fe0504cac8886d2ee57fc4d03df51955a16bce4b
Quincy Le <lexuqing8@gmail.com>
2018-09-17 16:12:49 -0400

    update test command & resolver syntax

--- tests/helpers/resolver.js
+++ tests/helpers/resolver.js
@@ -1,7 +1,7 @@
 import config from '../../config/environment'
 import Resolver from '../../resolver'
 
-const resolver = Resolver.create()
+const resolver = Resolver.create({namespace: 'resolver'})
 
 resolver.namespace = {
   modulePrefix: config.modulePrefix,
